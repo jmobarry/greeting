8d013d288b3f397d0186c981ab047305787fe3d0
jmobarry <jmobarry@montgomerycollege.edu>
2021-07-28 11:51:24 -0400

    Test Ready
    Updated the files to be testable.

--- hello.test.js
+++ hello.test.js
@@ -1,5 +1,7 @@
-var utils  = require('course-utilities');
-var hello = utils.load('./hello.js', 'hello');
+// var utils  = require('course-utilities');
+// var hello = utils.load('./hello.js', 'hello');
+
+const hello = require ('./hello.js')
 
 test('outputs the correct string', () => {
   expect(hello()).toBe("Hello World!");
